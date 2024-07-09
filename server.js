19af4fc6100e127dfffd334380a87212353182b4
tofa93 <tofig.valiyev1993@gmail.com>
2024-07-10 01:57:22 +0200

    Creating a server

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log("request made");
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        default:
+            path += '404.html'
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        }
+        else {
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost', () => {
