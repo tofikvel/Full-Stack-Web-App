a317511b266bb92e20ac7b0116436e4f8c8e6b6d
tofa93 <tofig.valiyev1993@gmail.com>
2024-07-11 01:28:35 +0200

    adding the express package

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
-})
+});
 
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', { root: __dirname });
-})
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
