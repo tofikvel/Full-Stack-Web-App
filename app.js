db2d64252d403f25acb43c2bc782a2ad7054c467
tofa93 <tofig.valiyev1993@gmail.com>
2024-07-12 02:11:05 +0200

    passing data through

--- app.js
+++ app.js
@@ -7,28 +7,33 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Shop' });
+    const items = [
+        { title: 'Coca-Cola', price: '$2.99', description: 'Refreshing carbonated drink' },
+        { title: 'Sprite', price: '$1.99', description: 'Refreshing carbonated drink' },
+        { title: 'Fanta', price: '$2.99', description: 'Refreshing carbonated drink' }
+    ];
+    res.render('index', { title: 'Shop', items: items });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: 'About' });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', { title: 'Contact' });
 });
 
 app.get('/product', (req, res) => {
-    res.render('product');
+    res.render('product', { title: 'Product' });
 });
 
 app.get('/shop/cart', (req, res) => {
-    res.render('cart');
+    res.render('cart', { title: 'Shopping cart' });
 });
 app.get('/comment', (req, res) => {
-    res.render('comment');
+    res.render('comment', { title: 'Comment' });
 });
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: 'Comment' });
 });
