bef46bc75bc916240d6c0b0170a354ec22db6054
tofa93 <tofig.valiyev1993@gmail.com>
2024-07-25 01:22:58 +0200

    added few more lines of code

--- app.js
+++ app.js
@@ -17,60 +17,27 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // register view engine
 app.set('view engine', 'ejs');
+
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.get('/add-item', (req, res) => {
-    const item = new Cart({
-        title: 'Sprite',
-        price: '$1.49',
-        description: 'Refreshing carbonated drink',
-        weight: '0.35L',
-        image: 'sprite'
-    });
-
-    item.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
 
-
-
-// route to get all products
-app.get('/all-products', (req, res) => {
-    Product.find()
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-// route to get a single product
-app.get('/single-product', (req, res) => {
-    Product.findById('6699b95fe51c3dd2ea27bb2a')
-        .then((data) => {
-            res.send(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-
-
-
-
+// Routes
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Main' });
+    res.redirect('/products');
 });
+
+app.get('/products', (req, res) => {
+    Product.find().sort({ createdAt: -1 })
+        .then((data) => {
+            res.render('index', { title: 'All Products', products: data })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
@@ -87,6 +54,7 @@ app.get('/product', (req, res) => {
 app.get('/cart', (req, res) => {
     res.render('cart', { title: 'Cart' });
 });
+
 app.get('/comment', (req, res) => {
     res.render('comment', { title: 'Comment' });
 });
